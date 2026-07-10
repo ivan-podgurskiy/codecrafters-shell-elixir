9d118f5e71784ac30c76e146fdabc9dfb0b47f39
Ivan Podgurskiy <ivan.podgurskiy@gmail.com>
2026-07-10 20:25:36 +0300

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -1,14 +1,19 @@
 defmodule CLI do
   require Logger
 
+  @builtin_commands ["exit", "echo", "type"]
+
   def main(_args) do
     run_terminal()
   end
 
   def run_terminal() do
     case IO.gets("$ ") do
+      :eof ->
+        :ok
+
       {:error, _} ->
-        IO.puts("\nBye")
+        :ok
 
       input ->
         {:ok, [command | args]} = parse_input(input)
@@ -31,5 +36,14 @@ defmodule CLI do
 
   def handle_command("exit", _), do: {:exit, :exit_command}
   def handle_command("echo", args), do: {:continue, Enum.join(args, " ")}
+
+  def handle_command("type", [command]) when command in @builtin_commands do
+    {:continue, "#{command} is a shell builtin"}
+  end
+
+  def handle_command("type", [command]) do
+    {:continue, "#{command}: not found"}
+  end
+
   def handle_command(command, _), do: {:continue, "#{command}: command not found"}
 end
